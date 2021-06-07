2c118ba26deab82e4401575b469d5bb04b9d8382
brooke <brookefuller96@knights.ucf.edu>
2021-06-06 23:04:46 -0400

    Initial commit

---
+++ src/main/java/Exercise11.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.lang.*;
+import static java.lang.System.*;
+
+
+public class Exercise11 {
+    public static void main(String[] args) {
+
+        System.out.println("How many euros are you exchanging? ");
+        Scanner input = new Scanner(in);
+        String euros = input.nextLine();
+        double euro = Double.parseDouble(euros);
+
+        System.out.println("What is the exchange rate? ");
+        Scanner input2 = new Scanner(in);
+        String rate = input2.nextLine();
+        double exchange_rate = Double.parseDouble(rate);
+
+        double eurotodollar = euro * exchange_rate;
+        double euro_rounded = Math.round(euro * 100.0) / 100.0;
+        double total_rounded = Math.round(eurotodollar * 100.0) / 100.0;
+
+        System.out.println(euro_rounded + " euros at an exchange rate of "
+                + exchange_rate + " is " + total_rounded + " U.S. dollars.");
+    }
+}
